2bede8c56f5f2a7d3e267a2f5cb115dcd6ed55c7
Communay Clément <ccommunay@gmail.com>
2023-05-17 19:29:23 +0200

    learn move camera 3d space wip

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera, ScrollControls, OrbitControls } from '@react-three/drei';
-import { ResizeObserver } from '@juggle/resize-observer';
+import React, { useRef, useLayoutEffect } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Box, ScrollControls, PerspectiveCamera, useScroll } from '@react-three/drei';
+import gsap from 'gsap';
 
-// import Stars from "./components/stars";
-import BackgroundGradient from "./components/background";
-import './App.css';
-import SphereCustom from "./components/sphere";
-import Stars from "./components/stars";
-import ScrollCamera from "./components/scroll_camera";
+function SpinningBox() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    // if (meshRef.current) {
+    //   meshRef.current.rotation.x += 0.01;
+    //   meshRef.current.rotation.y += 0.01;
+    // }
+  });
+
+  return (
+    <Box ref={meshRef}>
+      <meshStandardMaterial color="blue" />
+    </Box>
+  );
+}
+
+function ScrollableCamera() {
+  const cameraRef = useRef();
+  const scroll = useScroll();
+
+  const radius = 5; // la distance de la caméra par rapport à l'origine (le cube)
+
+  useFrame(() => {
+    if (cameraRef.current) {
+      const theta = 2 * Math.PI * scroll.offset; // convertit l'offset de défilement en un angle de rotation
+      cameraRef.current.position.x = radius * Math.sin(theta);
+      cameraRef.current.position.z = radius * Math.cos(theta);
+      // cameraRef.current.lookAt(0, 0, 0); // s'assure que la caméra est toujours tournée vers le cube
+    }
+  });
+
+  return <PerspectiveCamera ref={cameraRef} position={[0, 0, radius]} />;
+}
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <img src="/pictures/profile_picture.png" alt="Logo" className="logo" />
-        <h2>0ruj Portfolio</h2>
-      </header>
-      <Canvas
-        resize={{ polyfill: ResizeObserver }}
-        style={{ height: '100%', width: '100%', position: 'absolute', overflow: 'hidden' }}>
-        <ScrollControls pages={2}>
-          <BackgroundGradient />
-          <ScrollCamera />
-          <SphereCustom />
-          <Stars />
-        </ScrollControls>
-      </Canvas>
-    </div>
+    <Canvas style={{ height: '100%', width: '100%', overflow: 'hidden', position: 'absolute' }}>
+      <ambientLight intensity={0.5} />
+      <ScrollControls pages={3} damping={0.25}>
+        <SpinningBox />
+        <ScrollableCamera />
+      </ScrollControls>
+    </Canvas>
   );
 }
 
 export default App;
+
