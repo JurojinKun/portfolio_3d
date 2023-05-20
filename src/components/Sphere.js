ccdbde2e3f14332fadbd76ddd1f676351c312f51
0ruj <ccommunay@gmail.com>
2023-05-21 01:02:57 +0200

    Commit 21/05/23 . add logic satellites around sphere

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,18 +1,25 @@
 import React, { useRef, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
-import { Sphere } from '@react-three/drei';
+import { Sphere, Text } from '@react-three/drei';
 
 const SphereCustom = ({ scroll }) => {
     const meshRef = useRef();
     const lightRef = useRef();
     const materialRefs = useMemo(() => Array(2000).fill(0).map(() => React.createRef()), []);
 
+    const satellitesRef = useRef([]);
+
+    const onSatelliteClick = (index) => {
+        console.log(`Satellite ${index + 1} clicked`);
+        // Ajoutez votre logique ici
+    }
+
     useFrame(({ clock }) => {
         if (meshRef.current && lightRef.current) {
-            meshRef.current.rotation.x += 0.001
-            // meshRef.current.rotation.y += 0.001;
-            meshRef.current.rotation.z += 0.001
+            // meshRef.current.rotation.x += 0.001;
+            meshRef.current.rotation.y += 0.001;
+            meshRef.current.rotation.z += 0.001;
 
             // Change sphere color over time
             const elapsedTime = clock.getElapsedTime();
@@ -21,12 +28,31 @@ const SphereCustom = ({ scroll }) => {
 
             if (scroll.offset > 0) {
                 const startPosition = [1.5, -1, 7];
-                const endPosition = [0, -0.2, 4];
+                const endPosition = [0, 0.1, 3.5];
 
                 // Interpolation linéaire entre les positions de départ et de fin en fonction de offset
-                meshRef.current.position.x = startPosition[0] + scroll.offset * (endPosition[0] - startPosition[0]);
-                meshRef.current.position.y = startPosition[1] + scroll.offset * (endPosition[1] - startPosition[1]);
-                meshRef.current.position.z = startPosition[2] + scroll.offset * (endPosition[2] - startPosition[2]);
+                meshRef.current.position.x = (startPosition[0] + scroll.offset * (endPosition[0] - startPosition[0]));
+                meshRef.current.position.y = (startPosition[1] + scroll.offset * (endPosition[1] - startPosition[1]));
+                meshRef.current.position.z = (startPosition[2] + scroll.offset * (endPosition[2] - startPosition[2]));
+
+
+                // Déplacez les satellites autour de la sphère si la sphère est à sa position finale
+                if (meshRef.current.position.x <= (endPosition[0] + 0.2) &&
+                    meshRef.current.position.z <= (endPosition[2] + 0.2)) {
+                    satellitesRef.current.children.forEach((satellite, index) => {
+                        satellite.visible = true;
+                        const angle = (2 * Math.PI / satellitesRef.current.children.length) * index + clock.getElapsedTime() * 0.2;
+                        const distance = 2.7; // distance from the center of the sphere
+                        satellite.position.x = meshRef.current.position.x + distance * Math.cos(angle);
+                        satellite.position.y = meshRef.current.position.y + distance * Math.sin(angle);
+                        satellite.position.z = meshRef.current.position.z;
+                        satellite.material.color.set(color);
+                    });
+                } else {
+                    satellitesRef.current.children.forEach((satellite) => {
+                        satellite.visible = false;
+                    });
+                }
             }
         }
     });
@@ -59,6 +85,27 @@ const SphereCustom = ({ scroll }) => {
             <mesh ref={meshRef} position={[1.5, -1, 7]}>
                 {spheres}
             </mesh>
+            <group ref={satellitesRef}>
+                {[...Array(5)].map((_, i) => (
+                    <mesh
+                        key={i}
+                        visible={false}
+                        onClick={() => onSatelliteClick(i)}
+                    >
+                        <sphereGeometry args={[0.15, 32, 32]} />
+                        <meshPhongMaterial color="white" />
+                        <Text
+                            position={[0, -0.3, 0]}
+                            fontSize={0.12}
+                            color="white"
+                            textAlign="center"
+                            fontWeight="bold"
+                        >
+                            Satellite {i + 1}
+                        </Text>
+                    </mesh>
+                ))}
+            </group>
             <ambientLight ref={lightRef} intensity={0.4} />
         </>
     );
