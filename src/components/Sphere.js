e05ef0fa439fd199c167191484087bf37dca83b2
0ruj <ccommunay@gmail.com>
2023-06-01 00:52:42 +0200

    Commit 01/06/23 . delete logic/ui .obj satellite component

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -148,7 +148,6 @@ const SphereCustom = ({ scroll }) => {
                     position={pos}
                     onClick={() => onSatelliteClick(i)}
                     sectionName={satelliteSection(i)}
-                    modelPath={"/icones/Fichier_1.obj"}
                 />
             ))}
             <ambientLight ref={lightRef} intensity={0.4} />
