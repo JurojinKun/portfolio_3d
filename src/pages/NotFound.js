51e17b9a9b901fdc5453a333642cefb9432514e8
0ruj <ccommunay@gmail.com>
2023-06-18 12:34:44 +0200

    Commit 18/06/23 . ui/logic not found screen

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,22 +1,51 @@
+import '../css/BlackHole.css';
+
 import React from "react";
+import { useTranslation } from 'react-i18next';
+
+import notFoundAstro from '../assets/astro_notfound.png'
 
 const NotFound = () => {
+    const { t } = useTranslation();
+
     return (
         <div style={{
             height: '100vh',
             width: '100vw',
-            backgroundColor: '#050505',
+            margin: 0,
+            padding: 0,
+            backgroundColor: '#161616',
             display: 'flex',
             justifyContent: 'center',
-            alignItems: 'center'
+            alignItems: 'center',
+            flexDirection: "column"
         }}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: "50px"
+            }}>
+                <img src={notFoundAstro} alt='Not Found' style={{
+                    height: "250px",
+                    width: "150px",
+                    paddingInline: "30px"
+                }} />
+                <div className="black_hole">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                </div>
+            </div>
             <div style={{
                 color: 'white',
                 textAlign: "center",
                 fontSize: 23,
                 fontWeight: "bold",
             }}>
-                URL not found
+                {t("not_found.url_not_found")}
             </div>
         </div>
     );
