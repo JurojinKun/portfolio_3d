7f99021cfea9e10a7133b7ce38c6df34da08827b
Communay Clément <ccommunay@gmail.com>
2023-06-26 18:38:17 +0200

    Commit 26/06/23 - redux logic language wip

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './rootReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ["languageReducer"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer
+  reducer: persistedReducer
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
