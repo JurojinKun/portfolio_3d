6d4be65a875f639cb724528aa8dd6faaf0223e0c
0ruj <ccommunay@gmail.com>
2023-08-09 23:30:34 +0200

    Commit 09/09/23 - add new icons experiences part

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -11,10 +11,9 @@ import { useTranslation } from "react-i18next";
 import { useInView } from "react-intersection-observer";
 
 import { textVariant } from "../utils/motion";
-import starbucks from "../assets/experiences/starbucks.png";
-import tesla from "../assets/experiences/tesla.png";
-import shopify from "../assets/experiences/shopify.png";
-import meta from "../assets/experiences/meta.png";
+import dp from "../assets/experiences/dp.png";
+import pepite from "../assets/experiences/pepite.png";
+import groupeatlantic from "../assets/experiences/groupe-atlantic.png";
 import { fontTitleBold, fontBodyBold } from "../utils/fonts";
 
 const ExperienceCard = ({ experience }) => {
@@ -109,7 +108,7 @@ const Experiences = () => {
         {
             title: "Full stack Developer",
             company_name: "Meta",
-            icon: meta,
+            icon: dp,
             iconBg: "#E6DEDD",
             date: "Jan 2023 - Present",
             points: [
@@ -122,8 +121,8 @@ const Experiences = () => {
         {
             title: "Web Developer",
             company_name: "Shopify",
-            icon: shopify,
-            iconBg: "#383E56",
+            icon: pepite,
+            iconBg: "#E6DEDD",
             date: "Jan 2022 - Jan 2023",
             points: [
                 "Developing and maintaining web applications using React.js and other related technologies.",
@@ -135,7 +134,7 @@ const Experiences = () => {
         {
             title: "React Native Developer",
             company_name: "Tesla",
-            icon: tesla,
+            icon: groupeatlantic,
             iconBg: "#E6DEDD",
             date: "Jan 2021 - Feb 2022",
             points: [
@@ -145,19 +144,6 @@ const Experiences = () => {
                 "Participating in code reviews and providing constructive feedback to other developers.",
             ],
         },
-        {
-            title: "React.js Developer",
-            company_name: "Starbucks",
-            icon: starbucks,
-            iconBg: "#383E56",
-            date: "March 2020 - April 2021",
-            points: [
-                "Developing and maintaining web applications using React.js and other related technologies.",
-                "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-                "Implementing responsive design and ensuring cross-browser compatibility.",
-                "Participating in code reviews and providing constructive feedback to other developers.",
-            ],
-        }
     ];
 
     return (
