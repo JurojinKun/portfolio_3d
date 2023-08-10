31e0fc8f590d3d29b1ae92fada1ebb269d4bfb9f
0ruj <ccommunay@gmail.com>
2023-08-10 15:54:26 +0200

    Commit 10/08/23 - add paragraphs skills part

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -30,23 +30,23 @@ const Skills = () => {
     const contentSkills = [
         {
             title: "Mes débuts dans le développement",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum fbrughrughruhgrivnrgbruhrugntugrhgurbhhhththtcvfbgbgbgb gurgburghrugrughrugnrighrig hrigrhgirhgirghr ighrighrighrigri",
+            content: "J'ai fait mes débuts dans le monde du développement à l'âge de 18 ans en apprenant le Python. \n\nCette expérience m'ayant plus, j'ai voulu continuer en intégrant un cycle préparatoire intégré et enfin une école d'ingénieur : ISEN Toulon. \nDurant ces années, j'ai pu apprendre à comprendre, à analyser et réfléchir à un code simple, compréhensible et propre en apprenant les bases du développement sur des technologies comme le C. \n\nJ'ai aussi pu en apprendre davantage en faisant notamment un stage au sein du groupe Atlantic avec l'équipe réseau et cybersécurité.",
         },
         {
             title: "Mon apprentissage autodidacte",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            content: "Après une diplômation dans ma formation d'ingénieur en tant qu'ingénieur dans le traitement de l'image et l'intelligence artificielle et ne voyant pas ma future carrière dans ce domaine, j'ai décidé de m'orienter vers l'entrepreneuriat et le développement mobile et web de manière autodidacte avec le programme Pépite Toulon. \n\nJ'ai pu apprendre avec leur aide et par moi-même un peu plus sur le monde de l'entrepreneuriat et la programmation mobile avec notamment le framework Flutter durant une année.",
         },
         {
             title: "Années de développeur front",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            content: "Après cette année concentrée sur ma montée en compétences autodidacte, j'ai décidé de découvrir et de m'épanouir dans le monde du travail avec un premier CDI au sein d'une start-up. \n\nCes années-là m'ont permis d'en apprendre beaucoup sur le développement front en mobile en Flutter, mais aussi dans d'autres technologies comme par exemple le React native en participant à des projets pour des clients jusqu'à devenir Lead dev front. \n\nJ'ai pu aussi de mon côté durant mon temps libre, améliorer mes compétences en faisant des projets personnels que ce soit mobile ou web.",
         },
         {
             title: "Et pourquoi pas développeur back ?",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            content: "J'ai beaucoup parlé de ma montée en compétences dans le développement front, mais qu'en est-il du développement back ? \n\nJ'ai pu participé à des ajouts et modifications dans certains projets en faisant du Node js mais j'ai encore beaucoup à apprendre afin de devenir un développeur full stack.",
         },
         {
             title: "L'avenir...",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            content: "Quelle aventure déjà ! Et la suite alors ? \n\nComme je l'ai dit, j'aimerai dans un futur proche, devenir un développeur full stack et par la suite de ma carrière, toujours prendre du plaisir dans ce que je fais et apprendre toujours de nouvelles choses et de nouvelles technologies.",
         },
     ];
 
@@ -84,7 +84,7 @@ const Skills = () => {
                                     <div key={index} className="container-in-middle">
                                         <div className="container-in-in-midle">
                                             <h2 style={fontBodyBold("25px", "white")}>{skill.title}</h2>
-                                            <p style={fontBodyNormal("20px", "white")}>{skill.content}</p>
+                                            <p style={fontBodyNormal("20px", "white")} dangerouslySetInnerHTML={{ __html: skill.content.replace(/\n/g, '<br>') }}/>
                                         </div>
                                     </div>
                                 );
