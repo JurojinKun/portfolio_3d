bd02a6050a55df6d859ef61b20529f9f5cad67cc
0ruj <ccommunay@gmail.com>
2023-10-12 13:56:14 +0200

    Commit 12/10/23 - add logic language error modale - fix problem condition error modale

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -2,28 +2,30 @@ import '../css/ErrorModal.css';
 
 import React from 'react';
 
-const ErrorModal = ({ error, resetErrorBoundary }) => {
-  
-    if (error.message.includes("Cannot read properties of null (reading 'indexOf')" && error.stack && error.stack.includes("react-three-fiber"))) {
-      return (
-        <div className="error-modal">
-          <div className="error-modal-content">
-            <h3>Une erreur est survenue</h3>
-            <p>Si vous avez un bloqueur, veuillez essayer de désactiver et rafraîchir la page</p>
-          </div>
-        </div>
-      );
+const ErrorModal = ({ error }) => {
+    
+    var language = window.navigator.language || window.navigator.userLanguage;
+
+    if (error.message.includes("Cannot read properties of null (reading 'indexOf')") && error.stack && error.stack.includes("react-three-fiber")) {
+        return (
+            <div className="error-modal">
+                <div className="error-modal-content">
+                    <h3>{language.startsWith("fr") ? "Une erreur est survenue" : "An error has occurred"}</h3>
+                    <p>{language.startsWith("fr") ? "Si vous avez un bloqueur sur votre navigateur, veuillez essayer de le désactiver et rafraîchir la page" : "If you have a blocker on your browser, please try to deactivate it and refresh the page"}</p>
+                </div>
+            </div>
+        );
     }
-  
+
     // Pour d'autres types d'erreurs, vous pouvez avoir une gestion différente
     return (
-      <div className="error-modal">
-        <div className="error-modal-content">
-          <h3>Une erreur est survenue</h3>
-          <p>{error.message}</p>
+        <div className="error-modal">
+            <div className="error-modal-content">
+                <h3>{language.startsWith("fr") ? "Une erreur est survenue" : "An error has occurred"}</h3>
+                <p>{error.message}</p>
+            </div>
         </div>
-      </div>
     );
-  }
+}
 
 export default ErrorModal;
