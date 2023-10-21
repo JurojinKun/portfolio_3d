9574795c8a216f930f789fe09b49cb8bde466d7c
0ruj <ccommunay@gmail.com>
2023-10-21 23:11:55 +0200

    Commit 21/10/23 - new ui loading screen

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -2,15 +2,17 @@ import '../css/Loading.css';
 
 import React from "react";
 
-import astroLoading from "../assets/astro_loading.png";
-
 const Loading = () => {
     var language = window.navigator.language || window.navigator.userLanguage;
 
     return (
         <div className="loading-screen">
-            <img src={astroLoading} alt="Astronaut loading" />
-            <div>
+            <div className='atom'>
+                <div className='line line-1' />
+                <div className='line line-2' />
+                <div className='line line-3' />
+            </div>
+            <div className='content-loading'>
                 <span className="loading-text">{language.startsWith("fr") ? "Chargement" : "Loading"}</span>
                 <span className="ellipsis">
                     <div></div>
