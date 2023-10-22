604d902f890f2085bb958327a7cd174dfd5987b1
0ruj <ccommunay@gmail.com>
2023-10-23 00:25:05 +0200

    Commit 23/10/23 - ui/logic project modal wip

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -5,8 +5,35 @@ import { FaTimes } from 'react-icons/fa';
 import { motion, AnimatePresence } from "framer-motion";
 
 const ProjectModal = ({ project, imgProject, setSelectedProject }) => {
+
     return (
         <AnimatePresence>
+            <motion.div className="image-hover-container"
+                layoutId={project}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}>
+                <div className="container">
+                    <div className="middle">
+                        <div className="icon-close-hover-project" onClick={() => setSelectedProject(null)}>
+                            <FaTimes className="icon-close-project" />
+                        </div>
+                        <img src={imgProject} alt='Project img' className="item-img-project" />
+                        <span className="title-animated-item-project">{project}</span>
+                    </div>
+                    <div className="bottom-right" />
+                    <div className="bottom-left" />
+                    <div className="top-left" />
+                </div>
+            </motion.div>
+        </AnimatePresence>
+    );
+}
+
+export default ProjectModal;
+
+{/* <AnimatePresence>
             <motion.div className="animated-item-project"
                 layoutId={project}
                 initial={{ opacity: 0 }}
@@ -19,8 +46,4 @@ const ProjectModal = ({ project, imgProject, setSelectedProject }) => {
                 <img src={imgProject} alt='Project img' className="item-img-project" />
                 <span className="title-animated-item-project">{project}</span>
             </motion.div>
-        </AnimatePresence>
-    );
-}
-
-export default ProjectModal;
+        </AnimatePresence> */}
