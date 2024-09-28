03c0a94d34c0296dd97207b0be6e42bfc27dcd1c
0ruj <ccommunay@gmail.com>
2024-09-28 20:31:35 +0200

    - add cgi experience ASM app - optimize ui experiences part

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -2,8 +2,8 @@ import "../css/Experiences.css";
 
 import React from "react";
 import {
-    VerticalTimeline,
-    VerticalTimelineElement,
+  VerticalTimeline,
+  VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { motion } from "framer-motion";
@@ -17,145 +17,169 @@ import groupeatlantic from "../assets/experiences/groupe-atlantic.png";
 import cgi from "../assets/experiences/cgi.png";
 
 const ExperienceCard = ({ experience }) => {
-    return (
-        <VerticalTimelineElement
-            contentStyle={{
-                background: "#1A1A1A",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  #47CDD6" }}
-            iconStyle={{ background: experience.iconBg }}
-            icon={
-                <div style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "100%",
-                    height: "100%"
-                }}>
-                    <img
-                        src={experience.icon}
-                        alt={experience.company_name}
-                        style={{
-                            height: "60%",
-                            width: "60%",
-                            objectFit: "contain"
-                        }}
-                    />
-                </div>
-            }
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "#1A1A1A",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  #47CDD6" }}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%",
+          }}
         >
-            <div>
-                <h3
-                    className="fontBodyBoldExperiences"
-                    style={{
-                        margin: 0
-                    }}
-                >{experience.title}</h3>
-                <p
-                    className="fontBodyBoldExperiences"
-                    style={{
-                        margin: 0,
-                        fontWeight: "bold"
-                    }}
-                >
-                    {experience.company_name}
-                </p>
-                <p
-                    className="fontBodyBoldExperiences"
-                    style={{
-                        margin: 0
-                    }}
-                >
-                    {experience.date}
-                </p>
-            </div>
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            style={{
+              height: "60%",
+              width: "60%",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3
+          className="fontBodyBoldExperiences"
+          style={{
+            margin: 0,
+          }}
+        >
+          {experience.title}
+        </h3>
+        <p
+          className="fontBodyBoldExperiences"
+          style={{
+            margin: 0,
+            fontWeight: "bold",
+          }}
+        >
+          {experience.company_name}
+        </p>
+        <p
+          className="fontBodyBoldExperiences"
+          style={{
+            margin: 0,
+          }}
+        >
+          {experience.date}
+        </p>
+      </div>
 
-            <ul style={{
-                marginTop: "1.25rem",
-                listStyleType: "disc",
-                marginLeft: "1.25rem"
-            }}>
-                {experience.points.map((point, index) => (
-                    <li
-                        key={`experience-point-${index}`}
-                        className="fontBodyNormalExperiences"
-                    >
-                        {point}
-                    </li>
-                ))}
-            </ul>
-        </VerticalTimelineElement >
-    );
-}
+      <ul
+        style={{
+          marginTop: "1.25rem",
+          listStyleType: "disc",
+          marginLeft: "1.25rem",
+        }}
+      >
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="fontBodyNormalExperiences"
+            style={{ marginBottom: "2rem" }}
+          >
+            {point.includes("Référence:") || point.includes("Reference:") ? (
+              <>
+                <span>{point.split(/Référence|Reference/)[0]}</span>
+                <br />{" "}
+                <strong>
+                  <span style={{ textDecoration: "underline" }}>
+                    {point.match(/Référence|Reference/)[0]}
+                  </span>
+                  {point.split(/Référence|Reference/)[1]}
+                </strong>
+              </>
+            ) : (
+              <span>{point}</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
 
 const Experiences = ({ threshold }) => {
-    const { t } = useTranslation();
-    const { ref, inView } = useInView({
-        triggerOnce: true, // Change to false if you want the animation to trigger again whenever it comes in view
-        threshold: threshold
-    });
+  const { t } = useTranslation();
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Change to false if you want the animation to trigger again whenever it comes in view
+    threshold: threshold,
+  });
 
-    const experiences = [
-        {
-            title: t("experiences.title_experience_4"),
-            company_name: t("experiences.company_experience_4"),
-            icon: cgi,
-            iconBg: "#E6DEDD",
-            date: t("experiences.date_experience_4"),
-            points: [],
-        },
-        {
-            title: t("experiences.title_experience_1"),
-            company_name: t("experiences.company_experience_1"),
-            icon: dp,
-            iconBg: "#E6DEDD",
-            date: t("experiences.date_experience_1"),
-            points: [
-                t("experiences.point_1_experience_1"),
-                t("experiences.point_2_experience_1"),
-                t("experiences.point_3_experience_1"),
-                t("experiences.point_4_experience_1"),
-                t("experiences.point_5_experience_1")
-            ],
-        },
-        {
-            title: t("experiences.title_experience_2"),
-            company_name: t("experiences.company_experience_2"),
-            icon: pepite,
-            iconBg: "#E6DEDD",
-            date: t("experiences.date_experience_2"),
-            points: [
-                t("experiences.point_1_experience_2"),
-                t("experiences.point_2_experience_2")
-            ],
-        },
-        {
-            title: t("experiences.title_experience_3"),
-            company_name: t("experiences.company_experience_3"),
-            icon: groupeatlantic,
-            iconBg: "#E6DEDD",
-            date: t("experiences.date_experience_3"),
-            points: [
-                t("experiences.point_1_experience_3")
-            ],
-        },
-    ];
+  const experiences = [
+    {
+      title: t("experiences.title_experience_4"),
+      company_name: t("experiences.company_experience_4"),
+      icon: cgi,
+      iconBg: "#E6DEDD",
+      date: t("experiences.date_experience_4"),
+      points: [t("experiences.point_1_experience_4")],
+    },
+    {
+      title: t("experiences.title_experience_1"),
+      company_name: t("experiences.company_experience_1"),
+      icon: dp,
+      iconBg: "#E6DEDD",
+      date: t("experiences.date_experience_1"),
+      points: [
+        t("experiences.point_1_experience_1"),
+        t("experiences.point_2_experience_1"),
+        t("experiences.point_3_experience_1"),
+        t("experiences.point_4_experience_1"),
+        t("experiences.point_5_experience_1"),
+      ],
+    },
+    {
+      title: t("experiences.title_experience_2"),
+      company_name: t("experiences.company_experience_2"),
+      icon: pepite,
+      iconBg: "#E6DEDD",
+      date: t("experiences.date_experience_2"),
+      points: [
+        t("experiences.point_1_experience_2"),
+        t("experiences.point_2_experience_2"),
+      ],
+    },
+    {
+      title: t("experiences.title_experience_3"),
+      company_name: t("experiences.company_experience_3"),
+      icon: groupeatlantic,
+      iconBg: "#E6DEDD",
+      date: t("experiences.date_experience_3"),
+      points: [t("experiences.point_1_experience_3")],
+    },
+  ];
 
-    return (
-        <div className="experiences">
-            <motion.div ref={ref} animate={inView ? "show" : "hidden"} initial="hidden" variants={textVariant(0.2)} className="experiences-title">
-            {t("experiences.title")}
-            <p>{t("experiences.subtitle")}</p>
-            </motion.div>
-            <div className="experiences-content">
-                <VerticalTimeline>
-                    {experiences.map((experience, index) => {
-                        return <ExperienceCard key={index} experience={experience} />
-                    })}
-                </VerticalTimeline>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="experiences">
+      <motion.div
+        ref={ref}
+        animate={inView ? "show" : "hidden"}
+        initial="hidden"
+        variants={textVariant(0.2)}
+        className="experiences-title"
+      >
+        {t("experiences.title")}
+        <p>{t("experiences.subtitle")}</p>
+      </motion.div>
+      <div className="experiences-content">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => {
+            return <ExperienceCard key={index} experience={experience} />;
+          })}
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+};
 
 export default Experiences;
