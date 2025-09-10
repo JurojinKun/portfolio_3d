11715055248d7ec28f7832bb1db662f17372498d
0ruj <ccommunay@gmail.com>
2025-09-10 21:44:59 +0200

    -modify overview footer to fixed footer

--- src/pages/Home3D.js
+++ src/pages/Home3D.js
@@ -1,23 +1,24 @@
-import '../css/Home3D.css';
+import { useEffect } from "react";
 
-import MainSceneCanvas from '../canvas/MainSceneCanvas';
-import StarsCanvas from '../canvas/StarsCanvas';
-import FixedHeader from '../components/FixedHeader';
-import { useEffect } from 'react';
+import "../css/Home3D.css";
+import MainSceneCanvas from "../canvas/MainSceneCanvas";
+import StarsCanvas from "../canvas/StarsCanvas";
+import FixedHeader from "../components/FixedHeader";
+import FixedFooter from "../components/FixedFooter";
 
 const Home3D = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
 
-    useEffect(() => {
-        document.body.style.overflow = "hidden";
-    }, []);
-
-    return (
-        <div className='parent'>
-            <FixedHeader />
-            <StarsCanvas />
-            <MainSceneCanvas />
-        </div>
-    );
-}
+  return (
+    <div className="parent">
+      <FixedHeader />
+      <StarsCanvas />
+      <MainSceneCanvas />
+      <FixedFooter />
+    </div>
+  );
+};
 
 export default Home3D;
